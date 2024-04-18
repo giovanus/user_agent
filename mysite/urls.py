ad29cf22ac9b541d68bd92af18acd9325daa25b2
Mauril <giovanusamoussougbo@gmail.com>
2024-04-19 00:07:29 +0100

    twelveth

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,9 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from my_app import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('read/', views.read),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
